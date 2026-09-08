121196897e1f3840951c81d5c65e5fa276e24a9f
Aditya YV <adityayv802@gmail.com>
2026-09-07 22:11:18 -0500

    Removed debug statements

--- src/examplecan.cpp
+++ src/examplecan.cpp
@@ -9,14 +9,6 @@ ExampleCAN::ExampleCAN(const struct device * canDevice, const uint32_t targetIDL
 
 void ExampleCAN::readHandler(struct can_frame * msg) {
     if ((msg->id) == EXAMPLE_ID) {
-        uint8_t payload_len = can_dlc_to_bytes(msg->dlc);
-
-        LOG_INF("Received CAN Frame! ID: 0x%X, Length: %d bytes", msg->id, payload_len);
-
-        for (int i = 0; i < payload_len; i++) {
-            uint8_t byte_data = msg->data[i];
-            LOG_INF("Payload byte[%d]: 0x%02X", i, byte_data);
-        }
     }
 }
 
