9115f381ece6553fa45aee74313cca18cee93b54
Alexander teran <ateran@wiscc.edu>
2026-09-10 19:59:01 -0500

    file move

---
+++ inc/OTAP.h
@@ -0,0 +1,13 @@
+#ifndef OTAP_H
+#define OTAP_H
+#define slot0_partition 3
+
+#include <zephyr/dfu/flash_img.h>
+#include <zephyr/dfu/mcuboot.h>
+
+
+#include <stdint.h>
+#include <types.h>
+
+
+#endif
